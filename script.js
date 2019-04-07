125cfc2d1d509750a99cbd171e11fabb172c57ed
reinepearl <anjinhuang159@gmail.com>
2019-04-06 19:19:16 -0700

    added font size button and kaomoji buttons to all translated and original work
    also made footnote, contentParagraph2, contentParagraph3 id elements into if statements in script.jss

--- script.js
+++ script.js
@@ -1,29 +1,133 @@
+function increaseFontFunctionToMoreSmall(){
+	document.getElementById("contentParagraph").style.fontSize = "100%";
+	document.getElementById("contentChNav").style.fontSize = "100%";
+	document.getElementById("contentDewyNote").style.fontSize = "100%";
+	
+	var element1Exists = document.getElementById("contentTranNote");
+		if (element1Exists != null){
+			document.getElementById("contentTranNote").style.fontSize = "100%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph2");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph2").style.fontSize = "100%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph3");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph3").style.fontSize = "100%"
+		}
+	
+	var element1Exists = document.getElementById("contentFootnote");
+		if (element1Exists != null){
+			document.getElementById("contentFootnote").style.fontSize = "100%"
+		}
+}
+
+function increaseFontFunctionToSmall(){
+	document.getElementById("contentParagraph").style.fontSize = "110%";
+	document.getElementById("contentChNav").style.fontSize = "110%";
+	document.getElementById("contentDewyNote").style.fontSize = "110%";
+	
+	var element1Exists = document.getElementById("contentTranNote");
+		if (element1Exists != null){
+			document.getElementById("contentTranNote").style.fontSize = "110%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph2");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph2").style.fontSize = "110%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph3");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph3").style.fontSize = "110%"
+		}
+		
+	var element1Exists = document.getElementById("contentFootnote");
+		if (element1Exists != null){
+			document.getElementById("contentFootnote").style.fontSize = "110%"
+		}
+}
+
 function increaseFontFunctionToNormal(){
-	document.getElementById("contentBigParagraph").style.fontSize = "130%";
-	document.getElementById("contentBigTranNote").style.fontSize = "130%";
-	document.getElementById("contentBigChNav").style.fontSize = "130%";
-	document.getElementById("contentBigDewyNote").style.fontSize = "130%";
-	document.getElementById("contentBigFootnote").style.fontSize = "130%";
+	document.getElementById("contentParagraph").style.fontSize = "130%";
+	document.getElementById("contentChNav").style.fontSize = "130%";
+	document.getElementById("contentDewyNote").style.fontSize = "130%";
+	
+	var element1Exists = document.getElementById("contentTranNote");
+		if (element1Exists != null){
+			document.getElementById("contentTranNote").style.fontSize = "130%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph2");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph2").style.fontSize = "130%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph3");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph3").style.fontSize = "130%"
+		}
+		
+	var element1Exists = document.getElementById("contentFootnote");
+		if (element1Exists != null){
+			document.getElementById("contentFootnote").style.fontSize = "130%"
+		}
+	
 }
 
 function increaseFontFunctionToLarge(){
-	document.getElementById("contentBigParagraph").style.fontSize = "170%";
-	document.getElementById("contentBigTranNote").style.fontSize = "170%";
-	document.getElementById("contentBigChNav").style.fontSize = "170%";
-	document.getElementById("contentBigDewyNote").style.fontSize = "170%";
-	document.getElementById("contentBigFootnote").style.fontSize = "170%";
+	document.getElementById("contentParagraph").style.fontSize = "170%";
+	document.getElementById("contentChNav").style.fontSize = "170%";
+	document.getElementById("contentDewyNote").style.fontSize = "170%";
+	
+	var element1Exists = document.getElementById("contentTranNote");
+		if (element1Exists != null){
+			document.getElementById("contentTranNote").style.fontSize = "170%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph2");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph2").style.fontSize = "170%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph3");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph3").style.fontSize = "170%"
+		}
+	
+	var element1Exists = document.getElementById("contentFootnote");
+		if (element1Exists != null){
+			document.getElementById("contentFootnote").style.fontSize = "170%"
+		}
 }
 
 function increaseFontFunctionToMoreLarge(){
-	document.getElementById("contentBigParagraph").style.fontSize = "200%";
-	document.getElementById("contentBigTranNote").style.fontSize = "200%";
-	document.getElementById("contentBigChNav").style.fontSize = "200%";
-	document.getElementById("contentBigDewyNote").style.fontSize = "200%";
-	document.getElementById("contentBigFootnote").style.fontSize = "200%";
+	document.getElementById("contentParagraph").style.fontSize = "200%";
+	document.getElementById("contentChNav").style.fontSize = "200%";
+	document.getElementById("contentDewyNote").style.fontSize = "200%";
+	
+	var element1Exists = document.getElementById("contentTranNote");
+		if (element1Exists != null){
+			document.getElementById("contentTranNote").style.fontSize = "200%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph2");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph2").style.fontSize = "200%"
+		}
+	
+	var element1Exists = document.getElementById("contentParagraph3");
+		if (element1Exists != null){
+			document.getElementById("contentParagraph3").style.fontSize = "200%"
+		}
+		
+	var element1Exists = document.getElementById("contentFootnote");
+		if (element1Exists != null){
+			document.getElementById("contentFootnote").style.fontSize = "200%"
+		}
 }
-
-
-
 
 function incrementValueHappy(){
 	var value = document.getElementById("happyKaomoji").value;
